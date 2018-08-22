201ef8c109642af973b6dbc4cabce4d99083e91e
gentlegiantJGC <james_clare1@yahoo.co.uk>
2018-08-22 21:11:03 +0100

    heavily streamlined readForge

--- ACExplorer/ACUnity/readForge.py
+++ ACExplorer/ACUnity/readForge.py
@@ -1,67 +1,53 @@
 import os
+import struct
+import binascii
+import time
 from ACExplorer.misc import log
 from ACExplorer.misc.dataTypes import BE, BEHEX, LE2DEC
-
+from ACExplorer.misc.dataTypes import int32, uint64
 
 def readForge(fileTree, folder):
 	fileList = {}
-	for x in os.listdir(folder):
-		if x.endswith('.forge'):
-			log.info(__name__, 'Building file tree for {}'.format(x))
-			fileTree.insert('ACU', 'end', 'ACU|'+x, text=x)
-			f = open(folder+os.sep+x, 'rb')
-			fileLocation = 0
-			ftemp = f.read(21)
-			fileLocation += 21
-			if ftemp[0:8] != "scimitar":
+	for forgeFileName in os.listdir(folder):
+		if forgeFileName.endswith('.forge'):
+			log.info(__name__, 'Building file tree for {}'.format(forgeFileName))
+			fileTree.insert('ACU', 'end', 'ACU|{}'.format(forgeFileName), text=forgeFileName)
+			f = open(os.path.join(folder, forgeFileName), 'rb')
+			# header
+			if f.read(8) != 'scimitar':
 				continue
-			fileList[x] = {}
-			fileDataHeaderOffset = LE2DEC(ftemp, 13, 8) #read the offset to File Data Header, LE -> BE, hex, dec
-			f.read(fileDataHeaderOffset-21)
-			fileLocation += fileDataHeaderOffset-21
-			ftemp = f.read(44)
-			fileLocation += 44
-			fileDataOffset = LE2DEC(ftemp, 36, 8)
-			f.read(fileDataOffset-fileLocation)
-			fileLocation += fileDataOffset-fileLocation
-			ftemp = f.read(48)
-			fileLocation += 48
-			indexCount = LE2DEC(ftemp, 0, 4)
-			indexTableOffset = LE2DEC(ftemp, 8, 8)
-			nameTableOffset = LE2DEC(ftemp, 32, 8)
-			
-			f.read(indexTableOffset-fileLocation)
-			fileLocation += indexTableOffset-fileLocation
-			indexTable = f.read(indexCount*20)
-			fileLocation += indexCount*20
-			
-			f.read(nameTableOffset-fileLocation)
-			fileLocation += nameTableOffset-fileLocation
-			nameTable = f.read(indexCount*192)
-			fileLocation += indexCount*192
+			fileList[forgeFileName] = {}
+			f.seek(1, 1)
+			forgeFileVersion, fileDataHeaderOffset = struct.unpack('<iQ', f.read(12))
+			if forgeFileVersion != 27:
+				raise Exception('Unsupported Forge file format : "{}"'.format(forgeFileVersion))
+			f.seek(fileDataHeaderOffset+36)
+			fileDataOffset = struct.unpack('<q', f.read(8))[0]
+			f.seek(fileDataOffset)
+			# File Data
+			indexCount, indexTableOffset, fileDataOffset2, nameTableOffset, rawDataTableOffset = struct.unpack('<i4x2q8x2q', f.read(48))
+			f.seek(indexTableOffset)
+			indexTable = struct.unpack('<'+'q8si'*indexCount, f.read(20*indexCount))
+			f.seek(nameTableOffset)
+			nameTable = struct.unpack('<'+'i40x128s20x'*indexCount, f.read(192*indexCount))
 			forgeDatafiles = {}
-			for n in xrange(1, indexCount-1):
-				fileID = BEHEX(indexTable, 8+n*20, 8).upper()
-				fileList[x][fileID] = {					#file data id (matches the id in the file)
-					'rawDataOffset'	: LE2DEC(indexTable, n*20, 8),
-					'rawDataSize'	: LE2DEC(indexTable, 16+n*20, 4),
-					'fileName'	: BE(nameTable, n*192+44, 128).replace('\x00', '')			#file name
-				}
-				
-				if fileList[x][fileID]['fileName'] not in forgeDatafiles:
-					forgeDatafiles[fileList[x][fileID]['fileName']] = []
-				forgeDatafiles[fileList[x][fileID]['fileName']].append(fileID)
-					
-				
-				if fileList[x][fileID]['rawDataSize'] != LE2DEC(nameTable, n*192, 4):
+			for n in xrange(indexCount):
+				fileID = binascii.hexlify(indexTable[n*3+1]).upper()
+				fileName = nameTable[n*2+1].replace('\x00', '')
+				if indexTable[n*3+2] != nameTable[n*2]:
 					raise Exception('These should be the same. Is something wrong?')
-			
+				fileList[forgeFileName][fileID] = {  # file data id (matches the id in the file)
+					'rawDataOffset': indexTable[n*3],
+					'rawDataSize': indexTable[n*3+2],
+					'fileName': fileName
+				}
+
+				if fileName not in forgeDatafiles:
+					forgeDatafiles[fileName] = []
+				forgeDatafiles[fileName].append(fileID)
+
 			for fileName in sorted(forgeDatafiles, key=lambda v: v.lower()):
 				for fileID in forgeDatafiles[fileName]:
-					fileTree.insert('ACU|'+x, 'end', 'ACU|'+x+'|'+fileID, text=fileName)
-			
-			
-			
-			
+					fileTree.insert('ACU|{}'.format(forgeFileName), 'end', 'ACU|{}|{}'.format(forgeFileName, fileID), text=fileName)
 			f.close()
 	return fileList
