3222dfd81e2e14ab42c9f681c321adc0c14efa3d
gentlegiantJGC <james_clare1@yahoo.co.uk>
2018-10-26 13:45:12 +0100

    cleaned up inport script

--- ACExplorer/ACUnity/__init__.py
+++ ACExplorer/ACUnity/__init__.py
@@ -1,11 +1,16 @@
-gameIdentifier = 'ACU'
+from readForge_ import readForge
+
 from decompressDatafile_ import decompressDatafile
 from exportDataBlockModels_ import exportDataBlockModels
 
+from readFile_ import readFile
+
 from exportTexture_ import export_texture
+from exportModel_ import export_obj
 import formatFile
 from getMaterialIDs_ import getMaterialIDs
-from readFile_ import readFile
-from readForge_ import readForge
-from readModel_ import readModel
+
+from readModel_ import read_model
 from ACExplorer.ACUnity.exportFakes_ import exportFakes
+
+gameIdentifier = 'ACU'
