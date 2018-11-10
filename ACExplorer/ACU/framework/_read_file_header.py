07b2c4f0dc9416f2d99af606d1abf885760df6d3
gentlegiantJGC <james_clare1@yahoo.co.uk>
2018-11-10 15:13:49 +0000

    added a formatted file option

--- ACExplorer/ACU/framework/_read_file_header.py
+++ ACExplorer/ACU/framework/_read_file_header.py
@@ -1,4 +1,4 @@
-def read_file_header(file_object_data_wrapper):
-	file_object_data_wrapper.read_str(1)
-	file_object_data_wrapper.read_uint64()
-	return file_object_data_wrapper.read_uint32()
+def read_file_header(file_object_data_wrapper, out_file, indent_count):
+	file_object_data_wrapper.read_str(1, out_file, indent_count)
+	file_object_data_wrapper.read_uint_64(out_file, indent_count)
+	return file_object_data_wrapper.read_uint_32(out_file, indent_count)
--- ACExplorer/misc/file_loaders.py
+++ ACExplorer/misc/file_loaders.py
@@ -79,15 +79,35 @@ class DataTypeLoader:
 		self.plugins = {}
 
 	def get_data(self, file_object_data_wrapper, out_file=None, indent_count=0):
+		"""
+		Call this function in the right click methods as the start point
+		Will call get_data_recursive to get the actual data followed by the clever_format method to read the rest of the file
+		:param file_object_data_wrapper:
+		:param out_file:
+		:param indent_count:
+		:return: objects defined in the plugins
+		"""
 		if self.app.gameFunctions.gameIdentifier != self.game_identifier:
 			self.game_identifier = self.app.gameFunctions.gameIdentifier
 			self.plugins = {}
 			self.load_plugins()
 		if not isinstance(file_object_data_wrapper, self.app.misc.file_object.FileObjectDataWrapper):
 			raise Exception('file_object_data_wrapper is not of type FileObjectDataWrapper')
+		data = self.get_data_recursive(file_object_data_wrapper, out_file, indent_count)
+		file_object_data_wrapper.clever_format(out_file, indent_count)
+		return data
+
+	def get_data_recursive(self, file_object_data_wrapper, out_file, indent_count):
+		"""
+		Call this function in file reader methods to access other file types in the same file
+		:param file_object_data_wrapper:
+		:param out_file:
+		:param indent_count:
+		:return:
+		"""
 		file_type = self.app.gameFunctions.framework.read_file_header(file_object_data_wrapper, out_file, indent_count)
 		if file_type in self.plugins:
-			return self.plugins[file_type](self.app, file_object_data_wrapper, out_file, indent_count)
+			return self.plugins[file_type](self.app, file_object_data_wrapper, out_file, indent_count+1)
 		else:
 			raise Exception(f'File type {file_type:08X} does not have a file reader')
 
