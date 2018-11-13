771599fdd7d08a16eb7ed033a663b950dbb34982
gentlegiantJGC <james_clare1@yahoo.co.uk>
2018-11-13 20:34:54 +0000

    added a right click plugin to output the formatted file using the format modules

---
+++ ACExplorer/ACU/right_click_methods/format.py
@@ -0,0 +1,19 @@
+import os
+
+plugin_name = 'Format'
+plugin_level = 4
+file_type = '*'
+
+
+def plugin(app, file_id):
+	data = app.tempNewFiles(file_id)
+	if data is None:
+		app.log.warn(__name__, "Failed to find file {:016X}".format(file_id))
+		return
+	out_file = open(
+		os.path.join(
+			app.CONFIG['dumpFolder'],
+			f'{app.gameFunctions.gameIdentifier}_{data["fileName"]}_{file_id}.format'
+		), 'w'
+	)
+	app.read_file(data['rawFile'], out_file)
