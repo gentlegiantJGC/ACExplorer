934c5e0a19a3f02be86976f71ccb0a5cb049536f
gentlegiantJGC <james_clare1@yahoo.co.uk>
2019-02-02 20:43:39 +0000

    fixed decompress_all and added some logging

--- pyUbiForge/ACU/right_click_methods/decompress_all.py
+++ pyUbiForge/ACU/right_click_methods/decompress_all.py
@@ -3,5 +3,14 @@ plugin_level = 2
 
 
 def plugin(py_ubi_forge, forge_file_name, *_):
-	for file_id in py_ubi_forge.file_list[forge_file_name]:
-		py_ubi_forge.temp_files(file_id, forge_file_name, file_id)
+	datafile_count = len(py_ubi_forge.forge_files[forge_file_name].datafiles)
+	count = 0
+	for file_id in py_ubi_forge.forge_files[forge_file_name].datafiles:
+		try:
+			py_ubi_forge.temp_files(file_id, forge_file_name, file_id)
+		except:
+			continue
+		count += 1
+		if count % 1000:
+			py_ubi_forge.log.info(__name__, f"Decompressed {round(100*count/datafile_count, 2)}% of {datafile_count} datafiles")
+	py_ubi_forge.log.info(__name__, "Decompressed all files")
