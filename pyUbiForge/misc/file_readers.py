951c263d47f59137e2e1a04b1151f7dd7add31b2
gentlegiantJGC <james_clare1@yahoo.co.uk>
2019-02-08 13:28:48 +0000

    enforced type readers to be classes
    Type readers (the code that reads the raw file and packages it up for use) must now be a class that inherits BaseReader so that every reader is consistent and has file type packaged along with it

--- pyUbiForge/misc/file_readers.py
+++ pyUbiForge/misc/file_readers.py
@@ -4,11 +4,15 @@ from typing import Union, TextIO
 from pyUbiForge.misc.file_object import FileObjectDataWrapper
 
 
+class BaseReader:
+	file_type = None
+
+
 class FileReaderHandler:
 	def __init__(self, py_ubi_forge):
 		self.pyUbiForge = py_ubi_forge
 		self.game_identifier = None
-		self.plugins = {}
+		self.readers = {}
 
 	def __call__(self, file_object_data_wrapper: FileObjectDataWrapper, out_file: Union[None, FileObjectDataWrapper, TextIO] = None, indent_count: int = 0):
 		"""
@@ -19,7 +23,7 @@ class FileReaderHandler:
 		:param indent_count: The number of indents in out_file
 		:return: objects defined in the plugins
 		"""
-		self._load_plugins()
+		self._load_readers()
 		if not isinstance(file_object_data_wrapper, FileObjectDataWrapper):
 			raise Exception('file_object_data_wrapper is not of type FileObjectDataWrapper')
 		file_object_data_wrapper.read_str(self.pyUbiForge.game_functions.pre_header_length, out_file, indent_count)
@@ -41,40 +45,39 @@ class FileReaderHandler:
 
 		file_object_data_wrapper.read_id(out_file, indent_count)
 		file_type = file_object_data_wrapper.read_type(out_file, indent_count)
-		if file_type in self.plugins:
-			return self.plugins[file_type](self.pyUbiForge, file_object_data_wrapper, out_file, indent_count+1)
+		if file_type in self.readers:
+			return self.readers[file_type](self.pyUbiForge, file_object_data_wrapper, out_file, indent_count + 1)
 		else:
 			raise Exception(f'File type {file_type} does not have a file reader')
 
-	def _load_plugins(self):
+	def _load_readers(self):
 		"""Call this method to load plugins from disk. (This method is automatically called by the get method)"""
 		if self.pyUbiForge.game_identifier != self.game_identifier:
 			self.game_identifier = self.pyUbiForge.game_identifier
-			self.plugins = {}
+			self.readers = {}
 			for finder, name, _ in pkgutil.iter_modules([f'./pyUbiForge/{self.pyUbiForge.game_identifier}/type_readers']):
-				plugin = load_module(name, finder.path)
+				module = load_module(name, finder.path)
 
-				if not hasattr(plugin, 'file_type'):
+				if not hasattr(module, 'Reader') and issubclass(module.Reader, BaseReader):
+					self.pyUbiForge.log.warn(__name__, f'Failed loading {name} because "Reader" was either not defined, not a class or not a subclass of BaseReader')
+					continue
+
+				reader = module.Reader
+
+				if not hasattr(reader, 'file_type'):
 					self.pyUbiForge.log.warn(__name__, f'Failed loading {name} because "file_type" was not defined')
 					continue
-				elif not isinstance(plugin.file_type, str):
+				elif not isinstance(reader.file_type, str):
 					self.pyUbiForge.log.warn(__name__, f'Failed loading {name} because "file_type" was not a string')
 					continue
-				try:
-					file_type = plugin.file_type
-				except Exception as e:
-					self.pyUbiForge.log.warn(__name__, f'Failed loading {name} because parsing of "file_type" failed\n{e}')
-					continue
 
-				if not hasattr(plugin, 'plugin'):
-					self.pyUbiForge.log.warn(__name__, f'Failed loading {name} because "plugin" was not defined')
-					continue
+				file_type = reader.file_type
 
-				if file_type in self.plugins:
+				if file_type in self.readers:
 					self.pyUbiForge.log.warn(__name__, f'Skipping plugin "{name}" because a reader for this file type was already found')
 					continue
 				else:
-					self.plugins[file_type] = plugin.plugin
+					self.readers[file_type] = reader
 
 
 def load_module(name: str, path: str):
