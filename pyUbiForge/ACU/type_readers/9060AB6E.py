617c2a45f762365d4a766deaa5c364a8fe1bce49
gentlegiantJGC <james_clare1@yahoo.co.uk>
2019-02-09 20:30:56 +0000

    some more work on the entity format

--- pyUbiForge/ACU/type_readers/9060AB6E.py
+++ pyUbiForge/ACU/type_readers/9060AB6E.py
@@ -3,10 +3,8 @@ from pyUbiForge.misc.file_readers import BaseReader
 
 
 class Reader(BaseReader):
-	file_type = '71FDA747'
+	file_type = '9060AB6E'
 
 	def __init__(self, py_ubi_forge, file_object_data_wrapper: FileObjectDataWrapper, out_file, indent_count):
-		file_object_data_wrapper.read_str(4, out_file, indent_count)
-		file_object_data_wrapper.read_id(out_file, indent_count)
-		file_object_data_wrapper.read_str(35, out_file, indent_count)
+		file_object_data_wrapper.read_str(72, out_file, indent_count)
 		py_ubi_forge.read_file.get_data_recursive(file_object_data_wrapper, out_file, indent_count)
