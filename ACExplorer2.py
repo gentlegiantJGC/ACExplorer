8f353e0c459aa05c12b5161655f3da33d45e163d
gentlegiantJGC <james_clare1@yahoo.co.uk>
2019-02-20 17:16:35 +0000

    [UI] added the start of the backend of a search engine
    Added two classes that wrap around the QT Tree and TreeItem to make it easier to store data

--- ACExplorer2.py
+++ ACExplorer2.py
@@ -6,22 +6,87 @@
 from typing import Union
 import pyUbiForge
 from PySide2 import QtCore, QtGui, QtWidgets
+import sys
 
 
-class App:
+class TreeView(QtWidgets.QTreeWidget):
+	def __init__(self, py_ubi_forge, parent):
+		QtWidgets.QTreeWidget.__init__(self, parent)
+		self.pyUbiForge = py_ubi_forge
+		self._entries = {}
+		self._game_identifier = None
+
+	def load_game(self, game_identifier: str):
+		self._entries.clear()
+		# TODO: check that this removes entries from the tree
+		self._game_identifier = game_identifier
+		self.insert(game_identifier)
+		# TODO: populate the forge files and datafiles
+
+	def search(self, search_string: str) -> None:
+		self._entries[(None, None, None)].search(search_string)
+
+	def insert(self, entry_name: str, forge_file_name: str = None, datafile_id: int = None, file_id: int = None):
+		entry = TreeViewEntry(self.pyUbiForge, self, entry_name, forge_file_name, datafile_id, file_id)
+		self._entries[(forge_file_name, datafile_id, file_id)] = entry
+		if forge_file_name is not None:
+			if datafile_id is not None:
+				if file_id is not None:  # the fact that the ends of these align makes me very happy
+					self._entries[(forge_file_name, datafile_id, None)].addChild(entry)
+				else:
+					self._entries[(forge_file_name, None, None)].addChild(entry)
+			else:
+				self._entries[(None, None, None)].addChild(entry)
+
+
+class TreeViewEntry(QtWidgets.QTreeWidgetItem):
+	def __init__(self, py_ubi_forge, tree_view, entry_name: str, forge_file_name: str = None, datafile_id: int = None, file_id: int = None):
+		QtWidgets.QTreeWidgetItem.__init__(self, tree_view)
+		self.pyUbiForge = py_ubi_forge
+		self._entry_name = entry_name
+		self.setText(0, entry_name)
+		self._forge_file_name = forge_file_name
+		self._datafile_id = datafile_id
+		self._file_id = file_id
+		self._dev_search = [f'{attr:016X}' for attr in [datafile_id, file_id] if attr is not None]
+		self._dev_search += [''.join(attr[n:n+2] for n in reversed(range(0, 16, 2))) for attr in self._dev_search]
+
+	def search(self, search_string: str) -> bool:
+		if search_string == '' or any(search_string in attr for attr in [self._entry_name, self._forge_file_name]):
+			# if the string is empty or matches one of the parameters unhide self and children.
+			self.recursively_unhide_children()
+			return True
+		elif self.pyUbiForge.CONFIG['dev'] and any(search_string in attr for attr in self._dev_search):
+			# if in dev mode and matches one of the file ids unhide self and children
+			self.recursively_unhide_children()
+			return True
+		else:
+			shown = any([child.search(search_string) for child in self._children])
+			self.setHidden(not shown)
+			return shown
+
+	def recursively_unhide_children(self):
+		self.setHidden(False)
+		for index in range(self.childCount()):
+			self.child(index).recursively_unhide_children()
+
+
+
+class App(QtWidgets.QApplication):
 	"""This is the class that contains all the UI features for ACExplorer.
 
 	This class also sets up pyUbiForge.
 	"""
 	def __init__(self):
+		QtWidgets.QApplication.__init__(self)
 		self.pyUbiForge = pyUbiForge.PyUbiForgeMain()
 		self.log = self.pyUbiForge.log
 		self.log.info(__name__, 'Building GUI Window')
 
 		# set up main window
-		qt = QtWidgets.QApplication(sys.argv)
 		self.main_window = QtWidgets.QMainWindow()
 		self.main_window.setObjectName("MainWindow")
+		self.main_window.setWindowIcon(QtGui.QIcon('icon.ico'))
 		self.main_window.resize(809, 698)
 		self.central_widget = QtWidgets.QWidget(self.main_window)
 		self.central_widget.setObjectName("centralwidget")
@@ -44,9 +109,9 @@ class App:
 		self.vertical_layout.addLayout(self.horizontal_layout)
 
 		# file tree view
-		self.file_view = QtWidgets.QTreeWidget(self.central_widget)
+		self.file_view = TreeView(self.pyUbiForge, self.central_widget)
 		self.file_view.setObjectName("file_view")
-		item_0 = QtWidgets.QTreeWidgetItem(self.file_view)
+		self.file_view.load_game(self.game_select.currentText())
 		self.vertical_layout.addWidget(self.file_view)
 
 		# add the central widget
@@ -68,16 +133,14 @@ class App:
 		self.file_menu.addAction(self.options_button)
 		self.menubar.addAction(self.file_menu.menuAction())
 
-		self.translate()
+		self.translate_()
 		QtCore.QMetaObject.connectSlotsByName(self.main_window)
 
 		self.main_window.show()
-		sys.exit(qt.exec_())
+		sys.exit(self.exec_())
 
-	def translate(self):
+	def translate_(self):
 		self.main_window.setWindowTitle(QtWidgets.QApplication.translate("MainWindow", "ACExplorer"))
-		self.game_select.setItemText(0, QtWidgets.QApplication.translate("MainWindow", "ACU"))
-		self.file_view.headerItem().setText(0, QtWidgets.QApplication.translate("MainWindow", "1"))
 		__sortingEnabled = self.file_view.isSortingEnabled()
 		self.file_view.setSortingEnabled(False)
 		self.file_view.setSortingEnabled(__sortingEnabled)
@@ -86,6 +149,5 @@ class App:
 
 
 if __name__ == "__main__":
-	import sys
 	app = App()
 
