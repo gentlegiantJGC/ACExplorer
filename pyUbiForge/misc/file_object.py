efaf5deab88fadd7fc5613c8f0a5caadee79cdee
gentlegiantJGC <james_clare1@yahoo.co.uk>
2019-03-06 15:08:54 +0000

    [internal] added typing to file_object

--- pyUbiForge/misc/file_object.py
+++ pyUbiForge/misc/file_object.py
@@ -1,10 +1,11 @@
 import os
 import struct
 import numpy
+from typing import Union, IO, AnyStr
 
 
 class FileObject:
-	def __init__(self, path=None, mode='w', data=''):
+	def __init__(self, path: str = None, mode: str = 'w', data: AnyStr = ''):
 		self.path = None
 		self.mode = None
 		self._data = data
@@ -18,28 +19,26 @@ class FileObject:
 				self.mode = mode
 		self._file_pointer = 0
 
-	def tell(self):
+	def tell(self) -> int:
 		return self._file_pointer
 
-	def write(self, s):
+	def write(self, s: AnyStr):
 		self._data += s
 		self._file_pointer += len(s)
 
-	def read(self, length='end'):
+	def read(self, length: Union[str, int] = 'end'):
 		if length == 'end':
 			data = self._data[self._file_pointer:]
 			self._file_pointer = len(self._data)
 			return data
+		elif isinstance(length, int):
+			data = self._data[self._file_pointer:self._file_pointer + length]
+			self._file_pointer += length
+			return data
 		else:
-			try:
-				length = int(length)
-				data = self._data[self._file_pointer:self._file_pointer + length]
-				self._file_pointer += length
-				return data
-			except ValueError:
-				raise Exception(f'Unsupported type: "{type(length)}"')
+			raise Exception(f'Unsupported entry: "{length}"')
 
-	def seek(self, offset, whence=0):
+	def seek(self, offset: int, whence: int = 0):
 		if whence == 0:
 			self._file_pointer = offset
 		elif whence == 1:
@@ -47,7 +46,7 @@ class FileObject:
 		elif whence == 2:
 			self._file_pointer = len(self._data) - offset
 
-	def close(self, path=None, mode=None):
+	def close(self, path: Union[str, None] = None, mode: Union[str, None] = None):
 		if path is not None:
 			self.path = path
 		if mode is not None:
@@ -60,24 +59,25 @@ class FileObject:
 
 
 class FileObjectDataWrapper:
-	def __init__(self, py_ubi_forge, file_object, endianness='<'):
+	def __init__(self, py_ubi_forge, file_object: Union[FileObject, IO], endianness: str = '<'):
 		self.pyUbiForge = py_ubi_forge
 		self.file_object = file_object
+		assert endianness in ('<', '>')
 		self.endianness = endianness
 		self.indent_chr = '\t'
 
 	@classmethod
-	def from_binary(cls, py_ubi_forge, binary, endianness='<'):
+	def from_binary(cls, py_ubi_forge, binary: bytes, endianness: str = '<') -> 'FileObjectDataWrapper':
 		return cls(py_ubi_forge, FileObject(data=binary, mode='r'), endianness)
 
 	@classmethod
-	def from_file(cls, py_ubi_forge, path, endianness='<'):
+	def from_file(cls, py_ubi_forge, path: str, endianness: str = '<') -> 'FileObjectDataWrapper':
 		return cls(py_ubi_forge, open(path, 'rb'), endianness)
 
 	def close(self):
 		self.file_object.close()
 
-	def seek(self, offset, whence=0, out_file=None, indent_count=0):
+	def seek(self, offset: int, whence: int = 0, out_file: IO = None, indent_count: int = 0):
 		if out_file is None:
 			self.file_object.seek(offset, whence)
 		else:
@@ -105,11 +105,11 @@ class FileObjectDataWrapper:
 					out_file.write(f'Skipped back {abs(count)} bytes\n')
 					self.file_object.seek(offset, whence)
 
-	def out_file_write(self, val, out_file=None, indent_count=0):
+	def out_file_write(self, val: AnyStr, out_file: Union[IO, None] = None, indent_count: int = 0):
 		if out_file is not None:
 			out_file.write(f'{indent_count * self.indent_chr}{val}')
 
-	def _read_struct(self, out_file, indent_count, data_type, trailing_newline=True, extra_info=True):
+	def _read_struct(self, out_file: Union[IO, None], indent_count: int, data_type: str, trailing_newline: bool = True, extra_info: bool = True):
 		fmt = f'{self.endianness}{data_type}'
 		num_len = struct.calcsize(fmt)
 		binary = self.file_object.read(num_len)
@@ -130,40 +130,40 @@ class FileObjectDataWrapper:
 				out_file.write('\n')
 		return val
 
-	def read_bool(self, out_file=None, indent_count=0):
+	def read_bool(self, out_file: Union[IO, None] = None, indent_count: int = 0) -> bool:
 		return self._read_struct(out_file, indent_count, '?')
 
-	def read_int_8(self, out_file=None, indent_count=0):
+	def read_int_8(self, out_file: Union[IO, None] = None, indent_count: int = 0) -> int:
 		return self._read_struct(out_file, indent_count, 'b')
 
-	def read_uint_8(self, out_file=None, indent_count=0):
+	def read_uint_8(self, out_file: Union[IO, None] = None, indent_count: int = 0) -> int:
 		return self._read_struct(out_file, indent_count, 'B')
 
-	def read_int_16(self, out_file=None, indent_count=0):
+	def read_int_16(self, out_file: Union[IO, None] = None, indent_count: int = 0) -> int:
 		return self._read_struct(out_file, indent_count, 'h')
 
-	def read_uint_16(self, out_file=None, indent_count=0):
+	def read_uint_16(self, out_file: Union[IO, None] = None, indent_count: int = 0) -> int:
 		return self._read_struct(out_file, indent_count, 'H')
 
-	def read_int_32(self, out_file=None, indent_count=0):
+	def read_int_32(self, out_file: Union[IO, None] = None, indent_count: int = 0) -> int:
 		return self._read_struct(out_file, indent_count, 'i')
 
-	def read_uint_32(self, out_file=None, indent_count=0):
+	def read_uint_32(self, out_file: Union[IO, None] = None, indent_count: int = 0) -> int:
 		return self._read_struct(out_file, indent_count, 'I')
 
-	def read_float_32(self, out_file=None, indent_count=0):
+	def read_float_32(self, out_file: Union[IO, None] = None, indent_count: int = 0) -> int:
 		return self._read_struct(out_file, indent_count, 'f')
 
-	def read_int_64(self, out_file=None, indent_count=0):
+	def read_int_64(self, out_file: Union[IO, None] = None, indent_count: int = 0) -> int:
 		return self._read_struct(out_file, indent_count, 'q')
 
-	def read_uint_64(self, out_file=None, indent_count=0):
+	def read_uint_64(self, out_file: Union[IO, None] = None, indent_count: int = 0) -> int:
 		return self._read_struct(out_file, indent_count, 'Q')
 
-	def read_str(self, chr_len, out_file=None, indent_count=0):
+	def read_str(self, chr_len, out_file: Union[IO, None] = None, indent_count: int = 0) -> bytes:
 		return self._read_struct(out_file, indent_count, f'{chr_len}s')
 
-	def read_id(self, out_file=None, indent_count=0):
+	def read_id(self, out_file: Union[IO, None] = None, indent_count: int = 0) -> int:
 		file_id = self._read_struct(out_file, indent_count, self.pyUbiForge.game_functions.file_id_datatype, False, False)
 		if out_file is not None:
 			data = self.pyUbiForge.temp_files(file_id)
@@ -173,7 +173,7 @@ class FileObjectDataWrapper:
 				out_file.write('\t\t{data.file_name}\t{data.file_type}\n'.format(data=data))
 		return file_id
 
-	def read_type(self, out_file=None, indent_count=0):
+	def read_type(self, out_file: Union[IO, None] = None, indent_count: int = 0) -> str:
 		binary = self.file_object.read(self.pyUbiForge.game_functions.file_type_length)
 		if len(binary) != self.pyUbiForge.game_functions.file_type_length:
 			raise Exception('Reached End Of File')
@@ -182,7 +182,8 @@ class FileObjectDataWrapper:
 			out_file.write(f'{indent_count * self.indent_chr}{hex_string(binary)}\t\t{file_type}\t{self.pyUbiForge.game_functions.file_types.get(file_type, "Undefined")}\n')
 		return file_type
 
-	def read_struct(self, data_types, out_file=None, indent_count=0):
+	def read_struct(self, data_types: str, out_file: Union[IO, None] = None, indent_count: int = 0):
+		# data_types should not be prefixed with the endianness (this is added on for you)
 		fmt = f'{self.endianness}{data_types}'
 		binary = self.file_object.read(struct.calcsize(fmt))
 		if len(binary) != struct.calcsize(fmt):
@@ -192,7 +193,7 @@ class FileObjectDataWrapper:
 			out_file.write(f'{indent_count * self.indent_chr}{hex_string(binary)}\t\t{val}\n')
 		return val
 
-	def read_numpy(self, dtype, binary_size, out_file=None, indent_count=0):
+	def read_numpy(self, dtype, binary_size: int, out_file: Union[IO, None] = None, indent_count: int = 0):
 		binary = self.file_object.read(binary_size)
 		if len(binary) != binary_size:
 			raise Exception('Reached End Of File')
@@ -201,13 +202,13 @@ class FileObjectDataWrapper:
 			out_file.write(f'{indent_count * self.indent_chr}{hex_string(binary)}\t\t{val}\n')
 		return val
 
-	def read_rest(self, out_file=None, indent_count=0):
+	def read_rest(self, out_file: Union[IO, None] = None, indent_count: int = 0) -> bytes:
 		binary = self.file_object.read()
 		if out_file is not None:
 			out_file.write(f'{indent_count * self.indent_chr}{hex_string(binary)}\n')
 		return binary
 
-	def clever_format(self, out_file=None, indent_count=0):
+	def clever_format(self, out_file: Union[IO, None] = None, indent_count: int = 0):
 		if out_file is not None:
 			hex_str = []
 			might_be_a_file_type = ''.join(f'{b:02X}' for b in self.file_object.read(4)[::-1])
@@ -232,5 +233,5 @@ class FileObjectDataWrapper:
 		return
 
 
-def hex_string(binary):
+def hex_string(binary: bytes) -> str:
 	return ' '.join(f'{b:02X}' for b in binary)
