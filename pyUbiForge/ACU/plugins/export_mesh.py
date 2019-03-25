6d12767b5121e6265a41e1ddce8f8f4f69c0a375
gentlegiantJGC <james_clare1@yahoo.co.uk>
2019-03-25 16:40:28 +0000

    [ACU Plugin] Added new export mesh plugin
    This plugin merges the old "export obj" and "export collada" plugins into one. No longer will there be lots of plugins cluttering the plugin menu. Instead there will be a small number of more powerful plugins.

---
+++ pyUbiForge/ACU/plugins/export_mesh.py
@@ -0,0 +1,71 @@
+from pyUbiForge.misc import mesh
+from pyUbiForge.misc.plugins import BasePlugin
+from typing import Union, List
+
+
+class Plugin(BasePlugin):
+	plugin_name = 'Export Mesh'
+	plugin_level = 4
+	file_type = '415D9568'
+	_options = [
+		{
+			"Export Method": 'Wavefront (.obj)'
+		},
+		{
+			"Texture Type": 'DirectDraw Surface (.dds)'
+		}
+	]
+
+	def run(self, py_ubi_forge, file_id: Union[str, int], forge_file_name: str, datafile_id: int, options: list = None):
+		if options is not None:
+			self._options = options     # should do some validation here
+
+		# TODO add select directory option
+		save_folder = py_ubi_forge.CONFIG.get('dumpFolder', 'output')
+
+		data = py_ubi_forge.temp_files(file_id, forge_file_name, datafile_id)
+		if data is None:
+			py_ubi_forge.log.warn(__name__, f"Failed to find file {file_id:016X}")
+			return
+		model_name = data.file_name
+
+		if self._options[0]["Export Method"] == 'Wavefront (.obj)':
+			model = py_ubi_forge.read_file(data.file)
+			if model is not None:
+				obj_handler = mesh.ObjMtl(py_ubi_forge, model_name, save_folder)
+				obj_handler.export(model, model_name)
+				obj_handler.save_and_close()
+				py_ubi_forge.log.info(__name__, f'Exported {file_id:016X}')
+			else:
+				py_ubi_forge.log.warn(__name__, f'Failed to export {file_id:016X}')
+
+		elif self._options[0]["Export Method"] == 'Collada (.dae)':
+			obj_handler = mesh.Collada(py_ubi_forge, model_name, save_folder)
+			obj_handler.export(file_id, forge_file_name, datafile_id)
+			obj_handler.save_and_close()
+			py_ubi_forge.log.info(__name__, f'Exported {file_id:016X}')
+
+	def options(self, options: Union[List[dict], None]):
+		if options is None or (isinstance(options, list) and len(options) == 0):
+			return {
+				"Export Method": {
+					"type": "select",
+					"options": [
+						'Wavefront (.obj)',
+						'Collada (.dae)'
+					]
+				}
+			}
+		elif isinstance(options, list):
+			if len(options) == 1:
+				return {
+					"Texture Type": {
+						"type": "select",
+						"options": [
+							'DirectDraw Surface (.dds)'
+						]
+					}
+				}
+
+			elif len(options) == 2:
+				self._options = options
