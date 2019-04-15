80c1e2f2dbe66033ce49900d4b227c1f29f3edee
gentlegiantJGC <james_clare1@yahoo.co.uk>
2019-04-15 20:14:52 +0100

    [ACU Reader] Added the start of a reader for the entity group format
    This format looks almost identical to the entity format

--- pyUbiForge/ACU/type_readers/entity_group.py
+++ pyUbiForge/ACU/type_readers/entity_group.py
@@ -7,89 +7,42 @@ class Reader(BaseReader):
 	file_type = '3F742D26'
 
 	def __init__(self, py_ubi_forge, file_object_data_wrapper: FileObjectDataWrapper, out_file, indent_count):
-		# cheap method
-		# read top transformation matrix
-		# jump to next 29 8D 65 EC while in file
-		# jump to next 3B 96 6E 53
-		# read 3B 96 6E 53
-		check_byte_1 = file_object_data_wrapper.read_uint_8()
-		if check_byte_1 == 0:
-			py_ubi_forge.log.warn(__name__, 'checkbyte is not 3')
-			raise Exception
+		check_byte = file_object_data_wrapper.read_uint_8(out_file, indent_count)  # checkbyte 03 to continue (other stuff to not? have seen 00 with data after)
+		if check_byte == 0:
+			for _ in range(2):
+				py_ubi_forge.read_file.get_data_recursive(file_object_data_wrapper, out_file, indent_count)
+		file_object_data_wrapper.out_file_write('Transformation Matrix\n', out_file, indent_count)
 		self.transformation_matrix = file_object_data_wrapper.read_numpy(numpy.float32, 64, out_file, indent_count).reshape((4, 4), order='F')
+
+		file_object_data_wrapper.out_file_write('\n', out_file, indent_count)
+		count1 = file_object_data_wrapper.read_uint_32(out_file, indent_count)
+		if count1 > 10000:
+			py_ubi_forge.log.warn(__name__, 'error reading entity file')
+			# convert to an actual logger
+			raise Exception
+
+		self.nested_files = []
+
+		for _ in range(count1):
+			file_object_data_wrapper.out_file_write('\n', out_file, indent_count)
+			if file_object_data_wrapper.read_bytes(2, out_file, indent_count + 1) not in [b'\x04\x00', b'\x00\x01']:  # 04 00
+				raise Exception
+
+			self.nested_files.append(py_ubi_forge.read_file.get_data_recursive(file_object_data_wrapper, out_file, indent_count + 1))
+
 		file_object_data_wrapper.out_file_write('\n', out_file, indent_count)
 
-		self.file_id_list = []
+		file_object_data_wrapper.read_bytes(43, out_file, indent_count)
 
-		"""
-		filePointer = fIn.tell()
-		rawFile = fIn.read()
-		visualLoc = rawFile.find('\x29\x8D\x65\xEC')
+		# data layer filter
+		# 4 count, more data in here sometimes
+		for _ in range(3):
+			py_ubi_forge.read_file.get_data_recursive(file_object_data_wrapper, out_file, indent_count)
 
-		while visualLoc != -1:
-			fIn.seek(filePointer)
-			readStr(fIn, fOut, visualLoc - 8)
-
-			# jump to mesh
-			filePointer = fIn.tell()
-			rawFile = fIn.read()
-			bbloc = rawFile.find('\x3B\x96\x6E\x53')
-			if bbloc == -1:
-				return fileContainer
-			fIn.seek(filePointer)
-			readStr(fIn, fOut, bbloc - 8)
-
-			subFileContainer = py_ubi_forge.read_file.get_data_recursive(file_object_data_wrapper, out_file, indent_count)
-			if subFileContainer['meshID'] not in fileContainer['fileIDList']:
-				fileContainer['fileIDList'][subFileContainer['meshID']] = []
-			if len(subFileContainer['tm']) != len(subFileContainer['BB']):
-				raise Exception('should these be the same size?')
-			for a in range(len(subFileContainer['tm'])):
-				fileContainer['fileIDList'][subFileContainer['meshID']].append(
-					{
-						'tm': [fileContainer['transformationMtx']] + [subFileContainer['tm'][a]],
-						'BB': subFileContainer['BB']
-					}
-				)
-			if len(subFileContainer['tm']) == 0:
-				fileContainer['fileIDList'][subFileContainer['meshID']].append(
-					{
-						'tm': [fileContainer['transformationMtx']]
-					}
-				)
-
-			filePointer = fIn.tell()
-			rawFile = fIn.read()
-			visualLoc = rawFile.find('\x29\x8D\x65\xEC')
-
-		fIn.seek(filePointer)
-		"""
-
-		# checkByte1 = readInt(fIn, fOut, 1)
-		# if checkByte1 == 0:
-		# file_object_data_wrapper.read_id(out_file, indent_count)
-		# fileType2 = readType(fIn, fOut)
-		# recursiveFormat(py_ubi_forge, fileType2, fIn, fOut)
-		# file_object_data_wrapper.read_id(out_file, indent_count)
-		# fileType2 = readType(fIn, fOut)
-		# recursiveFormat(py_ubi_forge, fileType2, fIn, fOut)
-		# fileContainer['transformationMtx'] = [[],[],[],[]]
-		# for _ in range(4):
-		# for m in range(4):
-		# fileContainer['transformationMtx'][m].append(file_object_data_wrapper.read_float_32(out_file, indent_count))
-		# file_object_data_wrapper.out_file_write('\n', out_file, indent_count)
-		# count1 = file_object_data_wrapper.read_uint_32(out_file, indent_count)
-		# for _ in range(count1):
-		# file_object_data_wrapper.read_bytes(2, out_file, indent_count)
-		# file_object_data_wrapper.read_id(out_file, indent_count)
-		# fileType2 = readType(fIn, fOut)
-		# recursiveFormat(py_ubi_forge, fileType2, fIn, fOut)
-
-		# readStr(fIn, fOut, 43)
-
-		# for _ in range(3):
-		# file_object_data_wrapper.read_id(out_file, indent_count)
-		# fileType2 = readType(fIn, fOut)
-		# recursiveFormat(py_ubi_forge, fileType2, fIn, fOut)
-		# file_object_data_wrapper.out_file_write('\n', out_file, indent_count)
-		# return fileContainer
+		# 03 end file?
+		check_byte_2 = file_object_data_wrapper.read_uint_8(out_file, indent_count)
+		if check_byte_2 == 0:
+			py_ubi_forge.read_file.get_data_recursive(file_object_data_wrapper, out_file, indent_count)
+		elif check_byte_2 != 3:
+			raise Exception
+		file_object_data_wrapper.out_file_write('\n', out_file, indent_count)
