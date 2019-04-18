82f4ac4e8e6821a5a568506c84a027c30367cbc8
gentlegiantJGC <james_clare1@yahoo.co.uk>
2019-04-18 19:09:59 +0100

    [ACU Readers] Slight modifications to show read data

--- pyUbiForge/ACU/type_readers/D77FB524.py
+++ pyUbiForge/ACU/type_readers/D77FB524.py
@@ -1,5 +1,6 @@
 from pyUbiForge.misc.file_object import FileObjectDataWrapper
 from pyUbiForge.misc.file_readers import BaseReader
+from pyUbiForge.ACU.type_readers.entity import Reader as Entity
 
 
 class Reader(BaseReader):
@@ -7,10 +8,12 @@ class Reader(BaseReader):
 
 	def __init__(self, py_ubi_forge, file_object_data_wrapper: FileObjectDataWrapper):
 		file_object_data_wrapper.read_bytes(2)
-		py_ubi_forge.read_file.get_data_recursive(file_object_data_wrapper)
+		self.entity: Entity = py_ubi_forge.read_file.get_data_recursive(file_object_data_wrapper)
 		count1 = file_object_data_wrapper.read_uint_32()
+		file_object_data_wrapper.indent()
 		for _ in range(count1):
 			py_ubi_forge.read_file.get_data_recursive(file_object_data_wrapper)
+		file_object_data_wrapper.indent(-1)
 		count2 = file_object_data_wrapper.read_uint_32()
 		file_object_data_wrapper.indent()
 		file_object_data_wrapper.read_bytes(4*count2)
--- pyUbiForge/ACU/type_readers/fakes.py
+++ pyUbiForge/ACU/type_readers/fakes.py
@@ -1,15 +1,23 @@
 from pyUbiForge.misc.file_object import FileObjectDataWrapper
 from pyUbiForge.misc.file_readers import BaseReader
+from typing import List
+from pyUbiForge.ACU.type_readers.D77FB524 import Reader as Fake
 
 
 class Reader(BaseReader):
 	file_type = 'C69A7F31'
 
 	def __init__(self, py_ubi_forge, file_object_data_wrapper: FileObjectDataWrapper):
-		ent_count = file_object_data_wrapper.read_uint_32()
-		for _ in range(ent_count):
-			py_ubi_forge.read_file.get_data_recursive(file_object_data_wrapper)
-		ent_count2 = file_object_data_wrapper.read_uint_32()
-		for _ in range(ent_count2):
-			py_ubi_forge.read_file.get_data_recursive(file_object_data_wrapper)
+		fake_count = file_object_data_wrapper.read_uint_32()
+		self.fakes: List[Fake] = []
+		for _ in range(fake_count):
+			self.fakes.append(
+				py_ubi_forge.read_file.get_data_recursive(file_object_data_wrapper)
+			)
+		near_fake_count = file_object_data_wrapper.read_uint_32()
+		self.near_fakes = []
+		for _ in range(near_fake_count):
+			self.near_fakes.append(
+				py_ubi_forge.read_file.get_data_recursive(file_object_data_wrapper)
+			)
 		file_object_data_wrapper.read_bytes(1)
