29b2b5aaf3be662253ddb0bf89ee2ef48b597447
gentlegiantJGC <james_clare1@yahoo.co.uk>
2019-04-25 12:42:29 +0100

    [ACU Exporter] Mesh export debug tools
    Create a texture for each bone and mark the weighting for that bone on the texture.
    Send bones to blender

--- pyUbiForge/ACU/plugins/export_mesh.py
+++ pyUbiForge/ACU/plugins/export_mesh.py
@@ -2,6 +2,8 @@ from pyUbiForge.misc import mesh
 from pyUbiForge.misc.plugins import BasePlugin
 from typing import Union, List, Dict
 from multiprocessing.connection import Client
+from PIL import Image, ImageDraw
+import numpy
 
 
 class Plugin(BasePlugin):
@@ -50,12 +52,29 @@ class Plugin(BasePlugin):
 			model: mesh.BaseModel = py_ubi_forge.read_file(data.file)
 			if model is not None:
 				c = Client(('localhost', 6163))
+				cols = [Image.new('RGB', (1024, 1024), (128, 0, 0)) for _ in range(len(model.bones))]
+				r = 5
 				for mesh_index, m in enumerate(model.meshes):
 					c.send({
 						'type': 'MESH',
 						'verts': tuple(tuple(vert) for vert in model.vertices),
 						'faces': tuple(tuple(face) for face in model.faces[mesh_index][:m['face_count']])
 					})
+					for vtx in model.vert_table:
+						x, y = vtx['vt'].astype(numpy.float) / 2
+						for index, bone_index in enumerate(vtx['bn']):
+							if vtx['bw'][index] > 0:
+								draw = ImageDraw.Draw(cols[bone_index])
+								draw.ellipse((x - r, y - r, x + r, y + r), fill=(vtx['bw'][index], vtx['bw'][index], vtx['bw'][index]))
+				for index, im in enumerate(cols):
+					im.save(f'{save_folder}/{model_name}_{index}.png')
+					print(f'saved {save_folder}/{model_name}_{index}.png')
+
+				c.send({
+					'type': 'BONES',
+					'bone_id': [bone.bone_id for bone in model.bones],
+					'mat': [bone.transformation_matrix for bone in model.bones],
+				})
 
 	def options(self, options: Union[List[dict], None]) -> Union[Dict[str, dict], None]:
 		if options is None or (isinstance(options, list) and len(options) == 0):
