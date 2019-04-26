552748fab7183cce667ed152ee031dbe9c306544
gentlegiantJGC <james_clare1@yahoo.co.uk>
2019-04-26 12:57:13 +0100

    [Internals] Rewritten pyUbiForge.__init__ to not be a class

--- pyUbiForge/__init__.py
+++ pyUbiForge/__init__.py
@@ -11,120 +11,69 @@
 """
 
 import os
+import logging
 from typing import Union, Dict, List
-from pyUbiForge import ACU
-from pyUbiForge import misc as misc_
 from pyUbiForge.misc.forge import BaseForge
+from pyUbiForge.misc.config import CONFIG
+from pyUbiForge.misc.tempFiles2 import temp_files
+from pyUbiForge.misc.file_readers import file_reader_handler
+from pyUbiForge import ACU
+
+games = {
+	'ACU': ACU
+}
+game_functions: Union[ACU] = None
+forge_files: Dict[str, BaseForge] = {}
+read_file: file_reader_handler = file_reader_handler
 
 
-class _PyUbiForgeMain:
-	"""This is the main class that holds all the data for this package.
+def game_identifier() -> Union[str, None]:
+	"""Returns the game identifier for the game currently loaded.
 
-	To initiate the package first import it then create an instance of this class.
-	All the methods needed are either defined here or can be found within objects defined here.
+	Returns None if load_game has not been called.
 	"""
-	def __init__(self):
-		self._games = {
-			'ACU': ACU
-		}
-		self._CONFIG = misc_.Config()
-		self._game_functions = None
-		self._log = misc_.Logger(self)
-		self._temp_files = misc_.TempFilesContainer(self)
-		self._right_click_plugins = misc_.plugins.PluginHandler(self)
-		self._read_file = misc_.file_readers.FileReaderHandler(self)
-		self._forge_files = {}  # _forge_files is a dictionary mapping from str name of the forge file to a Forge class.
-
-	@property
-	def CONFIG(self) -> misc_.Config:
-		"""Returns the config class with data from config.json loaded if it exists."""
-		return self._CONFIG
-
-	@property
-	def game_functions(self):
-		"""Returns the package for the loaded game or None if load_game has not been called."""
-		return self._game_functions
-
-	@property
-	def log(self) -> misc_.Logger:
-		"""Returns the logging class."""
-		return self._log
-
-	@property
-	def misc(self) -> misc_:
-		return misc_
-
-	@property
-	def temp_files(self) -> misc_.TempFilesContainer:
-		"""Returns the temp_files class where all decompressed data is stored.
-
-		If you want a file, request it from this class and it will look for it, decompress it
-		if it exists, and return it. It will also deal with cleanup when nearing the memory cap.
-		"""
-		return self._temp_files
-
-	@property
-	def right_click_plugins(self) -> misc_.plugins.PluginHandler:
-		"""Returns a class that stores right click methods."""
-		return self._right_click_plugins
-
-	@property
-	def read_file(self) -> misc_.file_readers.FileReaderHandler:
-		"""Returns a class containing the code to read data from the files."""
-		return self._read_file
-
-	@property
-	def game_identifier(self) -> Union[str, None]:
-		"""Returns the game identifier for the game currently loaded.
-
-		Returns None if load_game has not been called.
-		"""
-		if self.game_functions is not None:
-			if hasattr(self.game_functions, 'game_identifier'):
-				return self.game_functions.game_identifier
-			else:
-				raise AttributeError
+	if game_functions is not None:
+		if hasattr(game_functions, 'game_identifier'):
+			return game_functions.game_identifier
 		else:
-			return None
+			raise AttributeError
+	else:
+		return None
 
-	@property
-	def forge_files(self) -> Dict[str, BaseForge]:
-		"""Returns the dictionary mapping file name string to Forge class."""
-		return self._forge_files
 
-	@property
-	def game_identifiers(self) -> List[str]:
-		return list(self._games.keys())
+def game_identifiers() -> List[str]:
+	return list(games.keys())
 
-	def load_game(self, game_identifier: str):
-		"""Call this with the identifier of the game you want to load.
 
-		This needs to be run after startup as no game is loaded to begin with
-		This is also what you should call if you want to switch between games.
-		Valid identifiers are defined in games at the top of this file.
-		"""
-		self.log.info(__name__, 'Loading Game Files.')
-		self.temp_files.clear()
-		if game_identifier in self._games:
-			self._game_functions = self._games.get(game_identifier)
-			self._forge_files = {}
-			if os.path.isdir(self.CONFIG.game_folder(game_identifier)):
-				for forge_file_name in os.listdir(self.CONFIG.game_folder(game_identifier)):
+def load_game(game_identifier_: str):
+	"""Call this with the identifier of the game you want to load.
+
+	This needs to be run after startup as no game is loaded to begin with
+	This is also what you should call if you want to switch between games.
+	Valid identifiers are defined in games at the top of this file.
+	"""
+	global game_functions
+	global forge_files
+	if game_identifier_ != game_identifier():
+		logging.info(__name__, f'Loading Game Files for {game_identifier_}.')
+		temp_files.clear()
+		if game_identifier_ in games:
+			game_functions = games.get(game_identifier_)
+			forge_files = {}
+			if os.path.isdir(CONFIG.game_folder(game_identifier_)):
+				for forge_file_name in os.listdir(CONFIG.game_folder(game_identifier_)):
 					if forge_file_name.endswith('.forge'):
-						self._forge_files[forge_file_name] = self.game_functions.forge.Forge(
-								self,
-								os.path.join(self.CONFIG.game_folder(game_identifier), forge_file_name),
-								forge_file_name
-							)
+						forge_files[forge_file_name] = game_functions.forge.Forge(
+							os.path.join(CONFIG.game_folder(game_identifier_), forge_file_name),
+							forge_file_name
+						)
 						yield forge_file_name
 
-			self.temp_files.load()
-		self.log.info(__name__, 'Finished Loading Game Files.')
-
-	def save(self):
-		"""Call this method to save the config file and light dictioary back to disk."""
-		self.CONFIG.save()
-		self.temp_files.save()
+			temp_files.load()
+		logging.info(__name__, 'Finished Loading Game Files.')
 
 
-main: _PyUbiForgeMain = _PyUbiForgeMain()
+def save():
+	"""Call this method to save the config file and light dictionary back to disk."""
+	CONFIG.save()
+	temp_files.save()
