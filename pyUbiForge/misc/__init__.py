441064bb0a5a138b910aaa9e8a6a22cbf92dc843
gentlegiantJGC <james_clare1@yahoo.co.uk>
2019-04-26 12:57:44 +0100

    [Internals] Removed imports from misc init

--- pyUbiForge/misc/__init__.py
+++ pyUbiForge/misc/__init__.py
@@ -1,6 +1,2 @@
 from .texture import BaseTexture, Material
-from .log import Logger
-from .decompress_ import decompress
-from .tempFiles2 import TempFilesContainer
-from .config_ import Config
 from . import file_object, mesh, plugins, file_readers
