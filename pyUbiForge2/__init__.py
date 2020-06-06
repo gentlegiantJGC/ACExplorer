af0efc0cd587f2b2c07a96f8b1a7c649761b7d98
gentlegiantJGC <james_clare1@yahoo.co.uk>
2020-06-06 20:56:15 +0100

    Cleaned up imports

--- pyUbiForge2/__init__.py
+++ pyUbiForge2/__init__.py
@@ -1,3 +1,5 @@
 import os
 
 CACHE_DIR = os.path.join(os.path.dirname(__file__), "cache")
+
+from pyUbiForge2.api import BaseGame, BaseForge, DataFile, BaseFile, FileDataWrapper
--- pyUbiForge2/api/__init__.py
+++ pyUbiForge2/api/__init__.py
@@ -1,2 +1,3 @@
 from pyUbiForge2.api.logging import log
 from pyUbiForge2.api.game import BaseGame, BaseForge, DataFile, BaseFile
+from pyUbiForge2.api.file_object import FileDataWrapper
