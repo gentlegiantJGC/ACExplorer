69687412fb9d072f8e2820e64344a1ba12889b79
gentlegiantJGC <james_clare1@yahoo.co.uk>
2020-06-06 16:15:40 +0100

    Added a base file class

---
+++ pyUbiForge2/api/game/file.py
@@ -0,0 +1,22 @@
+from pyUbiForge2.api.file_object import FileDataWrapper
+
+
+class BaseFile:
+    def __init__(
+            self,
+            file_id: int,
+            resource_type: int,
+            data_wrapper: FileDataWrapper
+    ):
+        self._file_id = file_id
+        self._resource_type = resource_type
+
+    @property
+    def file_id(self) -> int:
+        """The file id of the file in question"""
+        return self._file_id
+
+    @property
+    def resource_type(self) -> int:
+        """The file resource type of the file in question"""
+        return self._resource_type
--- pyUbiForge2/api/game/file_parser.py
+++
@@ -1,3 +0,0 @@
-class BaseFileParser:
-    def __init__(self):
-        pass
