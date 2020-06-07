9893e96fc3abc0ee263adfb74253ef2999675249
gentlegiantJGC <james_clare1@yahoo.co.uk>
2020-06-07 09:30:29 +0100

    Added a decompress with cache test and added logging to decompress test

--- tests/acu/decompress_with_cache.py
+++ tests/acu/decompress_with_cache.py
@@ -7,15 +7,15 @@ from pyUbiForge2.api import log
 
 
 @unittest.skipIf(ACUPath is None, reason="ACU Path not defined.")
-class DecompressTestCase(unittest.TestCase):
+class DecompressWithCacheTestCase(unittest.TestCase):
     def setUp(self) -> None:
         self._game = ACUGame(ACUPath)
 
     def test_decompress(self):
         start_time = time.time()
-        for forge_file in self._game.forge_files.values():
+        for forge_file_name, forge_file in self._game.forge_files.items():
             for data_file_id in forge_file.data_file_ids:
-                forge_file.get_decompressed_files(data_file_id)
+                self._game.get_file_bytes(data_file_id, forge_file_name, data_file_id)
             log.info(f"Finished decompressing {forge_file.file_name}")
         log.info(f"Finished decompressing all forge files in {round(time.time()-start_time)} seconds")
 
