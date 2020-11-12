734c2f8253369d66a14d474e7401d7ce64467998
gentlegiantJGC <james_clare1@yahoo.co.uk>
2020-11-12 20:17:02 +0000

    Simplified tests

---
+++ tests/test_acu.py
@@ -0,0 +1,15 @@
+import unittest
+from tests.game_test import BaseGameTestCase
+from tests import ACUPath as GamePath
+from pyUbiForge2.games import ACUGame as Game
+
+
+@unittest.skipIf(GamePath is None, reason=f"{Game.GameIdentifier} Path not defined.")
+class ACUTestCase(unittest.TestCase, BaseGameTestCase):
+    @classmethod
+    def setUpClass(cls):
+        return cls.setUpGame(Game, GamePath)
+
+
+if __name__ == '__main__':
+    unittest.main()
