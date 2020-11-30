20b8869c6953d286d19ba8832b56631f5653adbb
gentlegiantJGC <james_clare1@yahoo.co.uk>
2020-11-30 14:22:01 +0000

    Fixed AC3 forge class

--- pyUbiForge2/games/AC3/game.py
+++ pyUbiForge2/games/AC3/game.py
@@ -1,9 +1,9 @@
 from pyUbiForge2.api import BaseGame
-from pyUbiForge2.api.game.forge.forge_v1 import ForgeV1b
+from pyUbiForge2.api.game.forge.forge_v1 import ForgeV1c
 
 
 class AC3Game(BaseGame):
-    ForgeClass = ForgeV1b
+    ForgeClass = ForgeV1c
     GameIdentifier = "AC3"
     FileIDType = "Q"
     ResourceType = "I"
