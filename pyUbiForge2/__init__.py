4e73fb163736c3fcd298b27c3ff3e9e47684fe95
gentlegiantJGC <james_clare1@yahoo.co.uk>
2021-03-15 18:24:48 +0000

    Imported the game class map

--- pyUbiForge2/__init__.py
+++ pyUbiForge2/__init__.py
@@ -3,3 +3,5 @@ import os
 CACHE_DIR = os.path.join(os.path.dirname(__file__), "cache")
 
 from pyUbiForge2.api import BaseGame, BaseForge, DataFile, BaseFile, FileDataWrapper
+
+from .games import GAMES
